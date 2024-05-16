09084a09f69629ad3e0b41e02531123aba5ac263
Spokai3 <90892870+Spokai3@users.noreply.github.com>
2024-05-17 03:29:31 +0700

    Update index.js
    them access  control allow

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ require("dotenv").config();
 require("./db_connection");
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // CRUD
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
